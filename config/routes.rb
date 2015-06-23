ec01b42a4d3360dacaff1d1932a4d809fe527aa4
Juan Castillo <castillobgr@gmail.com>
2015-06-23 14:42:07 -0500

    Changing root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -54,6 +55,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  root 'application#hello'
 end
